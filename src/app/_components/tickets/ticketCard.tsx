3e9747e1d1251c24daac9034b5ea09b99415fb90
Osama <107386980+osSidd@users.noreply.github.com>
2024-03-26 09:36:03 +0530

    cut holes in ticket

--- src/app/_components/tickets/ticketCard.tsx
+++ src/app/_components/tickets/ticketCard.tsx
@@ -2,14 +2,14 @@ import ImageBox from "../cards/imageBox";
 
 export default function TicketCard(){
     return (
-        <div className="w-64 bg-white shadow-2xl overflow-hidden flex flex-col items-center" style={{height:'625px'}}>
+        <div className="w-64 bg-white shadow-2xl overflow-hidden flex flex-col items-center mix-blend-hard-light" style={{height:'625px'}}>
         <div className="px-3 pt-3">
             <ImageBox img="/ticket.jpeg" minWidth="226px"/>
         </div>
         <div className="flex justify-between items-center w-full mt-4">
-            <div className=" w-5 h-5 bg-white rounded-full" style={{marginLeft: '-10px'}}></div>
+            <div className=" w-5 h-5 bg-gray-200 rounded-full" style={{marginLeft: '-10px'}}></div>
             <div className=" w-52 h-1 border-t border-dashed border-gray-400"></div>
-            <div className=" w-5 h-5 bg-white rounded-full" style={{marginRight: '-10px'}}></div>
+            <div className=" w-5 h-5 bg-gray-200 rounded-full" style={{marginRight: '-10px'}}></div>
         </div>
         <p className=" font-medium text-lg mt-4">Las Vegas Aviators</p>
         <p className=" font-normal text-sm mt-2">OCT 15 | SUN | 4:30 PM</p>
--- src/app/_components/tickets/ticketsComponents.tsx
+++ src/app/_components/tickets/ticketsComponents.tsx
@@ -1,16 +1,34 @@
+import Image from "next/image";
 import TicketCard from "./ticketCard";
 
 export default function TicketsComponents(){
     return (
-        <div className=" w-8/12 my-24 mx-auto">
+        <div className=" w-10/12 my-24 mx-auto">
             <div className="text-center">
                 <h2 className="font-bold text-5xl">Collection Spotlight</h2>
                 <p className="font-normal text-sm mt-6">Discover extraordinary moments with our Spotlight Collection metatickets—exclusive access to premium events for an unforgettable experience. Grab yours today!</p>
             </div>
-        <div className="mt-12 flex justify-between">
+        <div className="mt-12 flex items-center justify-between">
+            <div className=" w-9 h-12 border border-blue-300 flex items-center justify-center cursor-pointer">
+                <Image
+                    src='/leftarrow.svg'
+                    height={11}
+                    width={19}
+                    alt="arrow-left"
+                />
+            </div>
             <TicketCard/>
             <TicketCard/>
             <TicketCard/>
+            <div className=" w-9 h-12 border border-blue-300 flex items-center justify-center cursor-pointer">
+                <Image
+                    src='/rightarrow.svg'
+                    height={11}
+                    width={19}
+                    alt="arrow-right"
+                />
+            </div>
+
         </div>
         </div>
     )
